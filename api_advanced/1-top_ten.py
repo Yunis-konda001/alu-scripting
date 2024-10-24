0bfeb453c78f7eb4d32719b1ad2eecc8e47ed673
Yunis-Konda001 <k.konda@alustudent.com>
2024-10-24 05:01:33 -0700

    top 10 hot pics

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,13 +5,22 @@ import requests
 
 
 def top_ten(subreddit):
-    """Main function"""
     URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in HOT_POSTS]
-    except Exception:
+        response = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json().get("data", {}).get("children", [])
+            if not data:
+                print(None)
+                return
+
+            for post in data:
+                print(post.get('data', {}).get('title'))
+        else:
+            print(None)
+
+    except requests.RequestException:
         print(None)
